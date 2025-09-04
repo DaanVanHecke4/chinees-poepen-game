7bc3dad96e00ade29f096dbcfdd17f4a581ca1b8
DaanVanHecke01 <daan.vanhecke@student.hogent.be>
2025-09-04 02:07:05 +0200

    Fix for server connection

--- server.js
+++ server.js
@@ -26,9 +26,6 @@ console.log('DATABASE_URL:', process.env.DATABASE_URL);
 // Databaseverbinding met Neon DB
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
 });
 
 // Initialiseer de database
